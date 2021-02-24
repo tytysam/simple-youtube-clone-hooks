63cdb540ebef0a02ba7fac8735b9991f81a2085a
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-24 13:05:13 -0700

    Wired up state for selectedVideo property. Then built VideoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import SearchBar from "./SearchBar.js";
 import VideoList from "./VideoList.js";
+import VideoDetail from "./VideoDetail.js";
 import youtube from "../apis/youtube.js";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -16,11 +17,19 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>{video.snippet.title}</div>
+    </div>
+  );
+};
+
+export default VideoDetail;
