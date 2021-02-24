2037bfd51fb63b1f11a3c04105fb1685e6396ed4
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-24 11:41:48 -0700

    Added form elements to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // * TODO: make sure to call callback from parent component
+  };
+
   render() {
-    return <div>SearchBar </div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
