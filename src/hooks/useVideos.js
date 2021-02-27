1570436fd32e503140fe17bbc5fbc42f87ecdbed
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-27 14:05:54 -0700

    Extracted video logic to a custom hook

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import youtube from "../apis/youtube.js";
+
+const useVideos = () => {
+  const [videos, setVideos] = useState([]);
+
+  const search = async (term) => {
+    const { data } = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    setVideos(data.items);
+  };
+
+  return { videos, search };
+};
+
+export default useVideos;
